986959fb6b2b0dd0919647ffe86197f2cd4d9d5a
christopherT02 <christopherterrier@hotmail.fr>
2023-07-08 16:10:36 +0200

    Log in full

--- src/main/java/com/example/netchill/loginController.java
+++ src/main/java/com/example/netchill/loginController.java
@@ -49,27 +49,34 @@ public class loginController {
     void click_button_submit(ActionEvent event) throws SQLException, IOException {
         boolean account_exist = false;
         String name_customer = new String();
-        if(txt_field_ID.equals("") || txt_field_password.equals(""))
+        if(txt_field_ID.getText().equals("") || txt_field_password.getText().equals(""))
         {
             System.out.println("Les champs ne peuvent pas être vide"); //idee afficher un label dynamique + améliorer les tests
         }
         else
         {
+            System.out.println("OUI");
 
             try {
-                Class.forName("com.mysql.jdbc.Driver");
+                System.out.println("61");
+                Class.forName("com.mysql.cj.jdbc.Driver");
                 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/netchill?useSSL=FALSE", "root", "");
+                System.out.println("64");
 
 
                 Statement stat = con.createStatement();
                 ResultSet rs = stat.executeQuery("SELECT * FROM `account` ");
+                System.out.println("65");
+
                 while (rs.next())
                 {
-                    System.out.println(rs.getString("custname"));
-                    if(txt_field_ID.equals(rs.getString("Email")) && txt_field_password.equals(rs.getString("Password")))
+                    System.out.println("Nous testons\n");
+                    if(txt_field_ID.getText().equals(rs.getString("Email")) && txt_field_password.getText().equals(rs.getString("Password")))
                     {
-                        ResultSet rs2 = stat.executeQuery("SELECT Name FROM customer WHERE customer.Email = rs.getString(\"Email\")");
-                        name_customer = rs2.getString("Name");
+
+                        System.out.println("OUIIII");
+                        ResultSet rs2 = stat.executeQuery("SELECT Name FROM customer WHERE customer.Email = '"+rs.getString("Email")+"'");
+                        while(rs2.next()) name_customer = rs2.getString("Name");
                         account_exist=true;
                     }
                 }
@@ -81,7 +88,9 @@ public class loginController {
 
             if(account_exist) //compte existant
             {
-                FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Detail_payment.fxml"));
+                System.out.println("OUI2");
+
+                FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
                 root=fxmlLoader.load();
                 HelloController controller = fxmlLoader.getController();
                 controller.updateLabel(name_customer); // Mettre à jour le texte du Label
@@ -90,6 +99,10 @@ public class loginController {
                 scene=new Scene(root);
                 lstage.setScene(scene);
                 lstage.show();
+            }
+            else
+            {
+                System.out.println("NONONNN");
             }
 
         }
