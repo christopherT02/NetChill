52c62c8c97fdeed7466aa7c65ce6cbaa63a3f52b
ilikenV2 <bananechargee@gmail.com>
2023-07-09 15:26:05 +0100

    add the poster visualisation when a movie is selected

--- src/main/java/com/example/netchill/moviesController.java
+++ src/main/java/com/example/netchill/moviesController.java
@@ -8,13 +8,13 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.sql.*;
 
 public class moviesController {
     @FXML
@@ -98,6 +98,32 @@ public class moviesController {
         txtTitle.setText(mov.getId_name());
         txtDescription.setText(mov.getDescription());
         txtTime.setText(mov.getTime() + "min");
+
+        //query to get the poster
+        String sqlQuery = "SELECT `Image_movie` FROM `movie` WHERE ID_name_movie = '"+mov.getId_name()+"'";
+
+        System.out.println(mov.getId_name());
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/netchill?useSSL=FALSE", "root", "");
+
+            Statement stat = con.createStatement();
+            ResultSet rs = stat.executeQuery(sqlQuery);
+            while(rs.next())
+            {
+                //convert blob into Image
+                Blob tempo = rs.getBlob("Image_movie");
+                InputStream tempo2 = tempo.getBinaryStream();
+                Image poster = new Image(tempo2);
+
+                imPoster.setImage(poster);
+            }
+            con.close();
+        } catch (Exception ee) {
+            System.out.println("non image " +ee);
+        }
+
 
 
     }
