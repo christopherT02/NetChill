0deb9c490570b0617e3f764d5e49017c2b3253b4
ilikenV2 <bananechargee@gmail.com>
2023-07-16 20:20:05 +0100

    stats

--- src/main/java/com/example/netchill/stats_Controller.java
+++ src/main/java/com/example/netchill/stats_Controller.java
@@ -1,5 +1,6 @@
 package com.example.netchill;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
@@ -41,6 +42,114 @@ public class stats_Controller {
     private Text txt_StatsSession;
 
 
+    public void cinema_stats(ActionEvent event)
+    {
+        txt_StatsCinema.setText("");
+
+        String sql = "SELECT `nb_sell_cinema` FROM `cinema` WHERE `name_cinema` = '"+ menu_Cinemas.getValue() +"'";
+        //add cinemas to the choicebox
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/netchill?useSSL=FALSE", "root", "");
+
+            Statement stat = con.createStatement();
+            ResultSet rs = stat.executeQuery(sql);
+
+            while(rs.next())
+            {
+                System.out.println(rs.getInt("nb_sell_cinema"));
+
+                //display result
+                txt_StatsCinema.setText(String.valueOf(rs.getInt("nb_sell_cinema")) + " tickets sold in this cinema.");
+            }
+
+            con.close();
+        } catch (Exception e2) {
+            System.out.println(e2);
+        }
+    }
+
+    public void room_stats(ActionEvent event)
+    {
+        txt_StatsRoom.setText("");
+
+        String sql = "SELECT `Nb_sell_place` FROM `room` WHERE `ID_nb_room` = '"+menu_Rooms.getValue()+"'";
+        //add cinemas to the choicebox
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/netchill?useSSL=FALSE", "root", "");
+
+            Statement stat = con.createStatement();
+            ResultSet rs = stat.executeQuery(sql);
+
+            while(rs.next())
+            {
+                System.out.println(rs.getInt("Nb_sell_place"));
+
+                //display result
+                txt_StatsRoom.setText(String.valueOf(rs.getInt("Nb_sell_place")) + " tickets sold for this room.");
+            }
+
+            con.close();
+        } catch (Exception e2) {
+            System.out.println(e2);
+        }
+    }
+
+    private void session_stats(ActionEvent event)
+    {
+        txt_StatsSession.setText("");
+
+        String sql = "SELECT * FROM `session` JOIN seat ON seat.ID_session = session.ID_session WHERE seat.ID_session = '"+ menu_Sessions.getValue() +"'";
+        //add cinemas to the choicebox
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/netchill?useSSL=FALSE", "root", "");
+
+            Statement stat = con.createStatement();
+            ResultSet rs = stat.executeQuery(sql);
+
+            int cmpt = 0;
+            while(rs.next())
+            {
+                cmpt++;
+            }
+
+            //display result
+            txt_StatsSession.setText(String.valueOf(cmpt) + " tickets sold for this session.");
+
+            con.close();
+        } catch (Exception e2) {
+            System.out.println(e2);
+        }
+    }
+
+    private void movie_stats(ActionEvent event)
+    {
+        txt_StatsMovie.setText("");
+
+        String sql = "SELECT `Nb_sale` FROM `movie` WHERE `ID_name_movie` =  '"+menu_Movies.getValue()+"'";
+        //add cinemas to the choicebox
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/netchill?useSSL=FALSE", "root", "");
+
+            Statement stat = con.createStatement();
+            ResultSet rs = stat.executeQuery(sql);
+
+            while(rs.next())
+            {
+                System.out.println(rs.getInt("Nb_sale"));
+
+                //display result
+                txt_StatsMovie.setText(String.valueOf(rs.getInt("Nb_sale")) + " tickets sold for this movie.");
+            }
+
+            con.close();
+        } catch (Exception e2) {
+            System.out.println(e2);
+        }
+    }
 
 
 
@@ -72,6 +181,7 @@ public class stats_Controller {
         } catch (Exception e2) {
             System.out.println(e2);
         }
+        menu_Cinemas.setOnAction(this::cinema_stats);
 
 
         sql = "SELECT `ID_nb_room` FROM `room` WHERE 1";
@@ -98,6 +208,7 @@ public class stats_Controller {
         } catch (Exception e2) {
             System.out.println(e2);
         }
+        menu_Rooms.setOnAction(this::room_stats);
 
 
         sql = "SELECT `ID_session` FROM `session` WHERE 1";
@@ -124,6 +235,7 @@ public class stats_Controller {
         } catch (Exception e2) {
             System.out.println(e2);
         }
+        menu_Sessions.setOnAction(this::session_stats);
 
 
         sql = "SELECT `ID_name_movie` FROM `movie` WHERE 1";
@@ -150,8 +262,12 @@ public class stats_Controller {
         } catch (Exception e2) {
             System.out.println(e2);
         }
+        menu_Movies.setOnAction(this::movie_stats);
 
     }
+
+
+
 
     @FXML
     public void update_customer_stats(Customer custom, Movie mov, ArrayList<Ticket> tickets, int nb_ticket_, int session_selected, int incrementor_, LocalDate date)
@@ -159,6 +275,6 @@ public class stats_Controller {
         netchill.send_all_info_netchill(custom,mov,tickets,nb_ticket_,session_selected,incrementor_,date);
         label_unuse.setText(netchill.getCustomer().getName_customer());
         label_unuse.setVisible(false);
-        System.out.println("DANS delete : "+netchill.getCustomer().getName_customer());
+        System.out.println("DANS delete : " + netchill.getCustomer().getName_customer());
     }
 }
